dacb787cd7d9f63145fda50ba5be2289a2182fca
antoine leloup <al@MacBook-Air.local>
2024-03-18 21:36:55 +0100

    correction bug fichier vide

---
+++ src/components/Content.jsx
@@ -0,0 +1,14 @@
+const Content = () => {
+  return (
+    <>
+      <div className="content">
+        <div className="MainContent">
+          <div className="article-1"></div>
+          <div className="article-2"></div>
+        </div>
+        <div className="SideContent"></div>
+      </div>
+    </>
+  );
+};
+export default Content;
---
+++ src/components/NavigationMenu.jsx
@@ -0,0 +1,21 @@
+const NavigationMenu = () => {
+  return (
+    <div className="nav">
+      <ul>
+        <li>
+          <a href="">Link 1</a>
+        </li>
+        <li>
+          <a href="">Link 2</a>
+        </li>
+        <li>
+          <a href="">Link 3</a>
+        </li>
+        <li className="lastLink">
+          <a href="">Link 4</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+export default NavigationMenu;
